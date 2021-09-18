63093f1df22861f56cfbe777ad9f4e00233c8501
Bartosso <barton547@gmail.com>
2021-09-18 15:20:50 +0800

    Query params error handling init

--- src/main/scala/forex/http/rates/QueryParams.scala
+++ src/main/scala/forex/http/rates/QueryParams.scala
@@ -1,15 +1,17 @@
 package forex.http.rates
 
 import forex.domain.Currency
-import org.http4s.QueryParamDecoder
-import org.http4s.dsl.impl.QueryParamDecoderMatcher
+import org.http4s.{ ParseResult, QueryParamDecoder }
+import org.http4s.dsl.impl.ValidatingQueryParamDecoderMatcher
 
 object QueryParams {
 
   private[http] implicit val currencyQueryParam: QueryParamDecoder[Currency] =
-    QueryParamDecoder[String].map(Currency.withNameInsensitive)
+    QueryParamDecoder[String].emap(
+      string => ParseResult.fromTryCatchNonFatal("unknown currency")(Currency.withNameInsensitive(string))
+    )
 
-  object FromQueryParam extends QueryParamDecoderMatcher[Currency]("from")
-  object ToQueryParam extends QueryParamDecoderMatcher[Currency]("to")
+  object FromQueryParam extends ValidatingQueryParamDecoderMatcher[Currency]("from")
+  object ToQueryParam extends ValidatingQueryParamDecoderMatcher[Currency]("to")
 
 }
--- src/main/scala/forex/http/rates/RatesHttpRoutes.scala
+++ src/main/scala/forex/http/rates/RatesHttpRoutes.scala
@@ -1,11 +1,14 @@
 package forex.http
 package rates
 
+import cats.data.ValidatedNel
 import cats.effect.Sync
+import cats.syntax.apply._
 import cats.syntax.flatMap._
+import forex.domain.Currency
 import forex.programs.RatesProgram
 import forex.programs.rates.{ Protocol => RatesProgramProtocol }
-import org.http4s.HttpRoutes
+import org.http4s.{ HttpRoutes, ParseFailure }
 import org.http4s.dsl.Http4sDsl
 import org.http4s.server.Router
 
@@ -17,10 +20,20 @@ class RatesHttpRoutes[F[_]: Sync](rates: RatesProgram[F]) extends Http4sDsl[F] {
 
   private val httpRoutes: HttpRoutes[F] = HttpRoutes.of[F] {
     case GET -> Root :? FromQueryParam(from) +& ToQueryParam(to) =>
-      rates.get(RatesProgramProtocol.GetRatesRequest(from, to)).flatMap(Sync[F].fromEither).flatMap { rate =>
-        Ok(rate.asGetApiResponse)
-      }
+      parseGetRatesRequest(from, to).fold(
+        _ => BadRequest("invalid params"),
+        request =>
+          rates.get(request).flatMap(Sync[F].fromEither).flatMap { rate =>
+            Ok(rate.asGetApiResponse)
+        }
+      )
   }
+
+  private def parseGetRatesRequest(
+      maybeFrom: ValidatedNel[ParseFailure, Currency],
+      maybeTo: ValidatedNel[ParseFailure, Currency]
+  ): ValidatedNel[ParseFailure, RatesProgramProtocol.GetRatesRequest] =
+    (maybeFrom, maybeTo).mapN(RatesProgramProtocol.GetRatesRequest)
 
   val routes: HttpRoutes[F] = Router(
     prefixPath -> httpRoutes
