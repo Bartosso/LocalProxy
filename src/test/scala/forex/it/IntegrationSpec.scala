ea5f196d9322605f2c44a12fc6e902fac5f6c337
Bartosso <barton547@gmail.com>
2021-09-20 21:23:08 +0800

    IntegrationSpec complete

--- src/test/scala/forex/it/IntegrationSpec.scala
+++ src/test/scala/forex/it/IntegrationSpec.scala
@@ -3,12 +3,16 @@ package forex.it
 import cats.effect.{ Blocker, ContextShift, IO, Timer }
 import com.dimafeng.testcontainers.{ ForAllTestContainer, GenericContainer }
 import forex.Application
-import forex.http.rates.server.models.out.GetApiResponse
+import forex.http.rates.server.models.out.{ GetApiResponse, ParseCurrencyError }
 import forex.TestCodecs._
 import forex.config.{ ApplicationConfig, Config }
 import forex.domain.Utils.jsonDecoder
+import forex.domain.Utils._
+import org.http4s.FormDataDecoder.formEntityDecoder
+import org.http4s.circe.CirceEntityCodec.circeEntityDecoder
 import org.http4s.client.blaze.BlazeClientBuilder
 import org.http4s.implicits.http4sLiteralsSyntax
+import org.scalatest.BeforeAndAfterAll
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import org.testcontainers.containers.wait.strategy.Wait
@@ -18,26 +22,57 @@ import scala.concurrent.ExecutionContext
 import scala.concurrent.ExecutionContext.global
 import scala.util.Try
 
-class IntegrationSpec extends AnyWordSpec with Matchers with ForAllTestContainer {
+class IntegrationSpec extends AnyWordSpec with Matchers with ForAllTestContainer with BeforeAndAfterAll {
 
   implicit lazy val context: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
   implicit lazy val timer: Timer[IO]          = IO.timer(ExecutionContext.global)
 
   lazy val config: ApplicationConfig = loadConfigAndSetTargetPort()
+  lazy val (client, releaseClient)   = BlazeClientBuilder[IO](global).resource.allocated.unsafeRunSync()
   val testUri                        = uri"http://localhost:8080/rates?from=JPY&to=USD"
 
   override val container: GenericContainer =
     GenericContainer("paidyinc/one-frame", exposedPorts = Seq(8080), waitStrategy = Wait.forHttp("/rates?pair=JPYUSD"))
 
+  override def afterAll(): Unit = releaseClient.unsafeRunSync()
+
   "Application" should {
     "work" in {
-      val (_, releaseApp)         = new Application[IO].resource(ExecutionContext.global, config).allocated.unsafeRunSync()
-      val (client, releaseClient) = BlazeClientBuilder[IO](global).resource.allocated.unsafeRunSync()
-      val requestResult           = client.get(testUri)(response => response.as[GetApiResponse])
-      val maybeGetApiResponse     = Try(requestResult.unsafeRunSync())
+      val (_, releaseApp)     = new Application[IO].resource(ExecutionContext.global, config).allocated.unsafeRunSync()
+      val requestResult       = client.get(testUri)(response => response.as[GetApiResponse])
+      val maybeGetApiResponse = Try(requestResult.unsafeRunSync())
       maybeGetApiResponse.isSuccess shouldBe true
       releaseApp.unsafeRunSync()
-      releaseClient.unsafeRunSync()
+      succeed
+    }
+
+    "return error if we pass invalid currency" in {
+      val (_, releaseApp) = new Application[IO].resource(ExecutionContext.global, config).allocated.unsafeRunSync()
+      val invalidUri      = uri"http://localhost:8080/rates?from=DSF&to=USD"
+      val errorMessages = client
+        .get(invalidUri) { response =>
+          response.status.code shouldBe 400
+          response.as[List[ParseCurrencyError]]
+        }
+        .unsafeRunSync()
+
+      errorMessages shouldBe List(ParseCurrencyError("from", "unknown currency"))
+      releaseApp.unsafeRunSync()
+      succeed
+    }
+
+    "return empty response if `from` and `to` have the same currency" in {
+      val (_, releaseApp) = new Application[IO].resource(ExecutionContext.global, config).allocated.unsafeRunSync()
+      val invalidUri      = uri"http://localhost:8080/rates?from=USD&to=USD"
+      val errorMessages = client
+        .get(invalidUri) { response =>
+          response.status.code shouldBe 200
+          response.as[List[Unit]]
+        }
+        .unsafeRunSync()
+
+      errorMessages shouldBe List()
+      releaseApp.unsafeRunSync()
       succeed
     }
   }
